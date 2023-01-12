28a8b3416e01884dfc41a1bcc6e36e892a78dfa6
AlonaStasyshyna <alena.stasishyna28@gmail.com>
2023-01-12 15:45:41 +0200

    Add endpoint for add notice by category

--- routes/api/notices.js
+++ routes/api/notices.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get("/:categoryName", controller.getNoticesByCategory);
 router.get("/:noticeId", controller.getNoticeById);
+router.put("/:categoryName", controller.addNoticeByCategory);
 
 module.exports = router;
