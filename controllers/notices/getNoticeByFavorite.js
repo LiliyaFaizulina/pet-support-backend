57aab2b2c4c4e780fffa843b5ce42fe2a98484b0
Danil-Bochkov <106696666+Danil-Bochkov@users.noreply.github.com>
2023-01-12 18:25:57 +0200

    Update

--- controllers/notices/getNoticeByFavorite.js
+++ controllers/notices/getNoticeByFavorite.js
@@ -2,8 +2,8 @@ const { Notice } = require("../../models/notices");
 const { HttpError } = require("../../helpers/HttpError");
 
 const getNoticeByFavorite = async (req, res) => {
-  const {_id: owner} = req.user;
-  const notice = await Notice.find({owner});
+  const { _id: owner } = req.user;
+  const notice = await Notice.find({owner, favorite: true});
   if (!notice) {
     throw HttpError(404);
   }
