2b545c39bbc4a1de6ec03638060d462a6d18392c
Лілія <lily.fed85@gmail.com>
2023-01-15 01:18:45 +0200

    update add notice controller

--- controllers/notices/addNoticeByCategory.js
+++ controllers/notices/addNoticeByCategory.js
@@ -1,16 +1,21 @@
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs/promises");
 const { Notice } = require("../../models/notices");
 
 const addNoticeByCategory = async (req, res) => {
-  const { categoryName } = req.params;
   const { _id: owner } = req.user;
+  const { path: tempUpload } = req.file;
+  const { url } = await cloudinary.uploader.upload(tempUpload);
+  const image = url;
 
   const result = await Notice.create({
-    category: categoryName,
-    owner,
     ...req.body,
+    image,
+    owner,
   });
+  fs.unlink(tempUpload);
 
-  res.status(201).json(result);
+  res.status(201).json({ notice: result });
 };
 
 module.exports = addNoticeByCategory;
