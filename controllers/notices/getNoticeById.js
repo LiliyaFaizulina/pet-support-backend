32c4392dfc534794e9274922c8b321037dc81c9d
Лілія <lily.fed85@gmail.com>
2023-01-19 16:37:30 +0200

    add users email and phone in notice

--- controllers/notices/getNoticeById.js
+++ controllers/notices/getNoticeById.js
@@ -3,7 +3,10 @@ const { HttpError } = require("../../helpers");
 
 const getNoticeById = async (req, res) => {
   const { noticeId } = req.params;
-  const notice = await Notice.findById(noticeId);
+  const notice = await Notice.findById(noticeId).populate(
+    "owner",
+    "email phone"
+  );
   if (!notice) {
     throw HttpError(404);
   }
--- controllers/users/auth.js
+++ controllers/users/auth.js
@@ -57,12 +57,12 @@ const login = async (req, res) => {
 
   const user = await User.findOne({ email });
   if (!user) {
-    throw HttpError(401, "Email or password is wrong");
+    throw HttpError(400, "Email or password is wrong");
   }
 
   const passwordCompare = await bcrypt.compare(password, user.password);
   if (!passwordCompare) {
-    throw HttpError(401, "Email or password is wrong");
+    throw HttpError(400, "Email or password is wrong");
   }
 
   const payload = {
