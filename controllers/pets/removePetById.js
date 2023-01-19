72eec0a19381c9e242624b3415a4e9089186561c
BigSiCorp <si.kocorp@gmail.com>
2023-01-19 16:54:00 +0200

    fix id

--- controllers/pets/removePetById.js
+++ controllers/pets/removePetById.js
@@ -2,12 +2,12 @@ const { Pet } = require("../../models/pets");
 const { HttpError } = require("../../helpers");
 
 const removePetById = async (req, res) => {
-  // const { id } = req.params;
+  const { id } = req.params;
 
   const { _id: owner } = req.user;
 
-  // const result = await Pet.findOneAndRemove({ _id: id, owner });
-  const result = await Pet.findOneAndRemove({ owner });
+  const result = await Pet.findOneAndRemove({ _id: id, owner });
+  // const result = await Pet.findOneAndRemove({ owner });
   if (!result) {
     throw HttpError(404, "Pet not found");
   }
